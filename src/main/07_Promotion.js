1d26546fab00350aac908d33346217168cfd512a
hyun-su <hyunsu6542@naver.com>
2024-12-02 00:29:35 +0900

    4th-1 modify

--- src/main/07_Promotion.js
+++ src/main/07_Promotion.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import "../styles/all.css"
 import './07_Promotion.css';
 
 function Promotion() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     return (
         <>
-            <div className="promotion-title">
+            {/* <div className="promotion-title">
                 <p>이제 시니핏을&nbsp;<span className="orange">특가</span>로 경험해보세요!</p>
-            </div>
+            </div> */}
             <div className="promotion">
-                <div className="application-consult" onClick={() => navigate('/sub4')}>
+                {/* <div className="application-consult" onClick={() => navigate('/sub4')}>
                     상담신청 →
-                </div>
+                </div> */}
             </div>
         </>
     );
