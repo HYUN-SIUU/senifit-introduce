50709117522a5a5662657c5e23bf4d45d3694d9f
hyun-su <hyunsu6542@naver.com>
2024-12-02 00:30:12 +0900

    4th-2 modify

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
             <p>본점 : 경기도 안산시 단원구 원포공원1로 59, 신명트윈타워 A동 5층 창업 5호</p>
             <p>서울지점 : 서울특별시 광진구 아차산로 272 B128 8호 (자양동, 더샵스타시티 스타트업존)</p>
             <p>사업자번호 : 327-86-03115</p>
-            <p>TEL : </p>
+            <p>TEL : 070-8065-7080</p>
             <p>E-MAIL : <span className="footer-email">teunteun.turtle@gmail.com</span></p>
             <p>Copyright 2024. SGEE.. All rights reserved</p>
         </footer>
