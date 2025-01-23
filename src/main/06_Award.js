105b12466e2cbeb98097ea87f0f3ee28219956ab
hyun-su <hyunsu6542@naver.com>
2025-01-24 00:05:15 +0900

    01.24 신문기사& 유튜브 아이콘& 웹페이지 검색 시 소개글

--- src/main/06_Award.js
+++ src/main/06_Award.js
@@ -6,6 +6,8 @@ import bgImage1 from '../img/06_award1.jpg';
 import bgImage2 from '../img/06_award2.jpg';
 import bgImage3 from '../img/06_award3.jpg';
 import bgImage4 from '../img/06_award4.png';
+import bgImage6 from '../img/06_award6.png';
+
 
 function Award() {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
@@ -25,23 +27,71 @@ function Award() {
 
     return (
         <>
-        <div className="award">
-            {/* 제목 */}
-            <h2 className="title">
-                시니핏은 이렇게 <span className="orange">인정</span>받고 있어요!
-            </h2>
+            <div className="award">
+                {/* 제목 */}
+                <h2 className="title">
+                    시니핏은 이렇게 <span className="orange">인정</span>받고 있어요!
+                </h2>
 
-            {/* <div className="video-container">
-                <video controls>
-                    <source src={videoFile} type="video/mp4" />
-                    해당 브라우저는 동영상을 지원하지 않습니다.
-                </video>
-            </div> */}
-        </div>
-        {/* 배경 사진 영역 */}
-        {isMobile ? (
-                <div className="award-images">
-                    <div>
+                <iframe 
+                    className='video-container'
+                    src="https://tv.naver.com/embed/66919186" 
+                    width="100%" 
+                    height="500" 
+                    frameborder="0" 
+                    title="senifit-video"
+                    allowfullscreen>
+                </iframe>
+            </div>
+            {/* 배경 사진 영역 */}
+            {isMobile ? (
+                <div className='award-images-box'>
+                    <div className="award-images">
+                        <div>
+                            <div className="background-item" onClick={() => openLink('https://isplus.com/article/view/isp202411260047')}>
+                                <img src={bgImage1} alt="배경 이미지 1" />
+                                <div className="overlay">
+                                    <p>시니핏, 시니어 운동 문화 형성 및 건강관리 위한 솔루션 제시해</p>
+                                    <span>출처 : 일간스포츠</span>
+                                </div>
+                            </div>
+                            <div className="background-item" onClick={() => openLink('https://www.incheonilbo.com/news/articleView.html?idxno=1259410')}>
+                                <img src={bgImage2} alt="배경 이미지 2" />
+                                <div className="overlay">
+                                    <p>'2024 안산 스타트 업 청년창업 경진대회' 성황리에 종료...우수상에 선정된 예비 창업가 ‘시니핏’ 팀</p>
+                                    <span>출처 : 인천일보</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="background-item" onClick={() => openLink('https://it.chosun.com/news/articleView.html?idxno=2023092126888')}>
+                                <img src={bgImage3} alt="배경 이미지 3" />
+                                <div className="overlay">
+                                    <p>‘서울 AI 영포럼’ 개최…청년 창업가들 AI 미래 보여줘...시니핏 등은 투자자들 앞에서 IR을 진행</p>
+                                    <span>출처 : IT조선</span>
+                                </div>
+                            </div>
+                            <div className="background-item" onClick={() => openLink('http://www.newsmaker.or.kr/news/articleView.html?idxno=159821')}>
+                                <img src={bgImage4} alt="배경 이미지 4" />
+                                <div className="overlay">
+                                    <p>시니어 서비스 분야 전문가로 우뚝 서겠다</p>
+                                    <span>출처 : 뉴스메이커</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                        <div className="background-item">
+                                <img src={bgImage6} alt="배경 이미지 6" />
+                            </div>
+                            <div className="background-item">
+                                <img src={bgImage6} alt="배경 이미지 6" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className='award-images-box'>
+                    <div className="award-images">
                         <div className="background-item" onClick={() => openLink('https://isplus.com/article/view/isp202411260047')}>
                             <img src={bgImage1} alt="배경 이미지 1" />
                             <div className="overlay">
@@ -56,8 +106,6 @@ function Award() {
                                 <span>출처 : 인천일보</span>
                             </div>
                         </div>
-                    </div>
-                    <div>
                         <div className="background-item" onClick={() => openLink('https://it.chosun.com/news/articleView.html?idxno=2023092126888')}>
                             <img src={bgImage3} alt="배경 이미지 3" />
                             <div className="overlay">
@@ -65,32 +113,20 @@ function Award() {
                                 <span>출처 : IT조선</span>
                             </div>
                         </div>
-                        <div className="background-item">
+                    </div>
+                    <div className="award-images">
+                        <div className="background-item" onClick={() => openLink('http://www.newsmaker.or.kr/news/articleView.html?idxno=159821')}>
                             <img src={bgImage4} alt="배경 이미지 4" />
+                            <div className="overlay">
+                                <p>시니어 서비스 분야 전문가로 우뚝 서겠다</p>
+                                <span>출처 : 뉴스메이커</span>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            ) : (
-                <div className="award-images">
-                    <div className="background-item" onClick={() => openLink('https://isplus.com/article/view/isp202411260047')}>
-                        <img src={bgImage1} alt="배경 이미지 1" />
-                        <div className="overlay">
-                            <p>시니핏, 시니어 운동 문화 형성 및 건강관리 위한 솔루션 제시해</p>
-                            <span>출처 : 일간스포츠</span>
+                        <div className="background-item">
+                            <img src={bgImage6} alt="배경 이미지 6" />
                         </div>
-                    </div>
-                    <div className="background-item" onClick={() => openLink('https://www.incheonilbo.com/news/articleView.html?idxno=1259410')}>
-                        <img src={bgImage2} alt="배경 이미지 2" />
-                        <div className="overlay">
-                            <p>'2024 안산 스타트 업 청년창업 경진대회' 성황리에 종료...우수상에 선정된 예비 창업가 ‘시니핏’ 팀</p>
-                            <span>출처 : 인천일보</span>
-                        </div>
-                    </div>
-                    <div className="background-item" onClick={() => openLink('https://it.chosun.com/news/articleView.html?idxno=2023092126888')}>
-                        <img src={bgImage3} alt="배경 이미지 3" />
-                        <div className="overlay">
-                            <p>‘서울 AI 영포럼’ 개최…청년 창업가들 AI 미래 보여줘...시니핏 등은 투자자들 앞에서 IR을 진행</p>
-                            <span>출처 : IT조선</span>
+                        <div className="background-item">
+                            <img src={bgImage6} alt="배경 이미지 6" />
                         </div>
                     </div>
                 </div>
--- src/main/11_SNSLinks.js
+++ src/main/11_SNSLinks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import kakao from '../img/kakao.png';
 import blog from '../img/blog.png';
 import insta from '../img/insta.png';
+import youtube from "../img/youtube.png";
 import "../styles/all.css"
 import './11_SNSLinks.css';
 
@@ -29,6 +30,13 @@ function SNSLinks() {
                 rel="noopener noreferrer"
             >
                 <img src={insta} alt="인스타 로고" className="sns-button" />
+            </a>
+            <a 
+                href="https://www.youtube.com/@%ED%8A%BC%ED%8A%BC%ED%95%9C%EA%B1%B0%EB%B6%81%EC%9D%B4-o9l/videos" 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                <img src={youtube} alt="유튜브브 로고" className="sns-button" />
             </a>
         </div>
     );
